2cd6026c58045ed0afefc411e1c6a1d10a24b526
Tito0ficial <titol.oficial@gmail.com>
2025-08-01 12:17:08 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header"
+import './index.css';
+
 function App() {
 
   return (
-    <h1>test</h1>
+    <Header/>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="content container">
+        <h1>DSFilter</h1>
+        <p>6 produto(s)</p>
+      </div>
+    </header>
+  );
+}
